8cc88e1f6e09e91f2ffc5bbf43b58b2d129a12c9
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-04-04 07:57:47 +0100

    Load BNC into memory and time process.

--- bnc.py
+++ bnc.py
@@ -1,1 +1,10 @@
 import nltk.corpus.reader.bnc
+import time
+
+
+start_time = time.perf_counter()
+BNC_data = nltk.corpus.reader.bnc.BNCCorpusReader(root='/home/ubuntu/ug-d/bncbaby/',
+                                            fileids=r'aca/\w*\.xml', # r'aca/\w*\.xml', # r'[a-z]{3}/\w*\.xml')
+                                            lazy=False)  # found here: https://github.com/nltk/nltk/issues/781 talk about how much more efficient it is
+time_taken = time.perf_counter() - start_time
+print('\n|| Successfully loaded the British National Corpus in {:.1f}'.format(time_taken), 'seconds. ||\n')
