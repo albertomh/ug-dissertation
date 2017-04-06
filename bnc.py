9241239e0ee53c51bcd324f0c48fa4c18bd771c8
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-04-06 10:49:57 +0100

    Created listall().

--- bnc.py
+++ bnc.py
@@ -10,7 +10,16 @@ fileids parameter.
 """
 start_time = time.perf_counter()
 BNC_data = nltk.corpus.reader.bnc.BNCCorpusReader(root='/home/ubuntu/ug-d/bncbaby/',
-                                            fileids=r'aca/\w*\.xml', # r'aca/\w*\.xml', # r'[a-z]{3}/\w*\.xml')
-                                            lazy=False)  # found here: https://github.com/nltk/nltk/issues/781 talk about how much more efficient it is
+                                                  fileids=r'aca/\w*\.xml',  # r'aca/\w*\.xml', # r'[a-z]{3}/\w*\.xml')
+                                                  lazy=False)  # found here: https://github.com/nltk/nltk/issues/781 talk about how much more efficient it is
 time_taken = time.perf_counter() - start_time
 print('\n|| Successfully loaded the British National Corpus in {:.1f}'.format(time_taken), 'seconds. ||\n')
+
+
+# ==========================================================================================================
+def listall():
+
+    start_time = time.perf_counter()
+
+    sent_counter = 0
+    file_counter = len(BNC_data.fileids())
