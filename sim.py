c5da36ad3e708dfe0e4042ce1f61b90e27afe88e
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-05-17 08:39:24 +0100

    Fixed spacing.

--- sim.py
+++ sim.py
@@ -1,6 +1,7 @@
 from gensim import models
 import time
 
+
 start_time = time.perf_counter()
 print('\nLoading vectors...\n')
 w = models.KeyedVectors.load_word2vec_format('/home/ubuntu/sim/CBOW|skipgram.bin', binary=True)
